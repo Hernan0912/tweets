7153e444ad082a523b7f71c0ee5ea831e12f18ed
Hernan <rhblanco62@gmail.com>
2024-07-23 00:29:07 -0300

    first commir

---
+++ main.js
@@ -0,0 +1,89 @@
+//Variables
+
+const boton = document.querySelector(".boton")
+const tweets = document.querySelector("#tweets")
+const form = document.querySelector("form")
+
+let arrayTweets = []
+
+//Eventos
+boton.addEventListener("click", agregarTweet)
+
+document.addEventListener("DOMContentLoaded", () => {
+    arrayTweets = JSON.parse(localStorage.getItem("arrayTweets")) || []
+    console.log("arrayTweets: ",arrayTweets)
+    crearHtml()
+})
+
+//Funciones
+function agregarTweet(e){
+    e.preventDefault()
+
+    const texto = document.querySelector(".textArea").value
+
+    if(texto.trim() == ""){
+        const contenedorBoton = document.querySelector(".contenedorBoton")
+        const error = document.createElement("p")
+        error.classList.add("error")
+        error.textContent= "No se puede ingresar un tweet vacío"
+        contenedorBoton.appendChild(error)
+        setTimeout(() => {
+            contenedorBoton.removeChild(contenedorBoton.lastChild)
+        }, 3000);
+        return
+    }
+
+    const historial = {
+        id:Date.now(),
+        texto
+    }
+
+    arrayTweets = [...arrayTweets, historial]
+    crearHtml()
+    form.submit()
+}
+
+function crearHtml(){
+    limpiaHtml()
+    if(arrayTweets.length > 0){
+        arrayTweets.forEach(element => {
+            const botonEliminar = document.createElement("a")
+            botonEliminar.classList.add("botonEliminar")
+            botonEliminar.textContent = "Eliminar"
+
+            // botonEliminar.addEventListener("click",borrarTweet)
+
+            botonEliminar.onclick = () => {
+                borrarTweet(element.id)
+            }
+
+            const li = document.createElement("li")
+
+            li.textContent = element.texto
+            li.appendChild(botonEliminar)
+
+            tweets.appendChild(li)
+
+        });
+    }
+    console.log("arrayTweets: ",arrayTweets)
+    guardarEnStorage("arrayTweets", arrayTweets)
+}
+
+function borrarTweet(id){
+    console.log("Borrando ",id)
+    arrayTweets = arrayTweets.filter(elemtent => elemtent.id !== id)
+    crearHtml()
+}
+
+//Limpio el html
+function limpiaHtml(){
+    while(tweets.firstChild){
+        tweets.removeChild(tweets.firstChild)
+    }
+}
+
+//Local Storage
+function guardarEnStorage(llave, valor){
+    localStorage.setItem(llave,JSON.stringify(valor))
+}
